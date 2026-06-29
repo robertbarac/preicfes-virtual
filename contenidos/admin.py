8031477c0bb9d723dba25e87b047e9c9a1776212
Robert Barrios <ryba.9125@gmail.com>
2026-06-28 21:16:40 -0500

    eliminar posts y contenidos

--- contenidos/admin.py
+++ contenidos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post, BloqueContenido
 
-# Register your models here.
+
+class BloqueContenidoInline(admin.StackedInline):
+    model = BloqueContenido
+    extra = 0
+    fields = ('tipo', 'orden', 'contenido_texto', 'archivo_imagen', 'url')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'modulo', 'tema', 'estado', 'orden', 'creador', 'fecha_creacion')
+    list_filter = ('estado', 'modulo', 'tema__materia')
+    search_fields = ('titulo',)
+    ordering = ('modulo', 'orden')
+    inlines = [BloqueContenidoInline]
+
+
+@admin.register(BloqueContenido)
+class BloqueContenidoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'tipo', 'orden', 'post')
+    list_filter = ('tipo',)
+    search_fields = ('post__titulo',)
